dd54c565cecb96807c4cd13118febb8f1400a712
keegcb <keegcb@gmail.com>
2023-04-02 11:49:27 -0400

    Initial commit

---
+++ app/src/main/java/com/example/uptoyou/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.uptoyou;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private static final int ERROR_DIALOG_REQUEST = 9001;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public boolean correctServices(){
+        Log.d(TAG, "correctServices: checking google services version");
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+
+        if(available == ConnectionResult.SUCCESS){
+            //correct services are installed and users can make map requests
+            Log.d(TAG, "correctServices: Google Play Services are working");
+            return true;
+        }
+        else if (GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            //error occurred but is fixable
+            Log.d(TAG, "correctServices: an error occurred but it can be fixed");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this, available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }
+        else{
+            Toast.makeText(this, "Map requests cannot be made", Toast.LENGTH_LONG).show();
+        }
+        return false;
+    }
+}
