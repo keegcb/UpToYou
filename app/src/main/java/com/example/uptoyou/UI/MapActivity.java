150009359b853a3f507cc597aaaf09d073d20822
keegcb <keegcb@gmail.com>
2023-04-03 11:44:50 -0400

    Updated compile SDK and min SDK to support needed libraries

--- app/src/main/java/com/example/uptoyou/UI/MapActivity.java
+++ app/src/main/java/com/example/uptoyou/UI/MapActivity.java
@@ -8,11 +8,22 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.example.uptoyou.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 
-public class MapActivity extends AppCompatActivity {
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback{
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Toast.makeText(this, "Map is Ready", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onMapReady: map is ready");
+        mMap = googleMap;
+    }
 
     private static final String TAG = "MapActivity";
 
@@ -20,19 +31,27 @@ public class MapActivity extends AppCompatActivity {
     private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
 
+    //variables
     private boolean mLocationPermissionsGranted = false;
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        getLocationPermission();
+
     }
 
     private void initMap(){
+        Log.d(TAG, "initMap: Initializing map");
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(MapActivity.this);
     }
 
     private void getLocationPermission(){
+        Log.d(TAG, "getLocationPermission: Getting location permissions");
         String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
         if(ContextCompat.checkSelfPermission(this.getApplicationContext(), FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
             if(ContextCompat.checkSelfPermission(this.getApplicationContext(), COURSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
@@ -42,10 +61,14 @@ public class MapActivity extends AppCompatActivity {
                 ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
             }
         }
+        else {
+            ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
+        }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.d(TAG, "onRequestPermissionResult: Verifying needed location permissions are in place");
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         mLocationPermissionsGranted = false;
         switch (requestCode) {
@@ -54,13 +77,18 @@ public class MapActivity extends AppCompatActivity {
                     for (int i = 0; i < grantResults.length; i++) {
                         if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
                             mLocationPermissionsGranted = false;
+                            Log.d(TAG, "onRequestPermissionResult: Location permissions failed.");
                             return;
                         }
                     }
                     mLocationPermissionsGranted = true;
+                    Log.d(TAG, "onRequestPermissionResult: Location permissions granted.");
                     //initialize map
+                    initMap();
                 }
             }
         }
     }
+
+
 }
