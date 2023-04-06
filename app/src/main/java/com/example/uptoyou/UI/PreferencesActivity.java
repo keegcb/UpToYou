ef4334dc7272ae37b2667115c475d5da1028aece
keegcb <keegcb@gmail.com>
2023-04-06 15:00:00 -0400

    Created preferences activity

---
+++ app/src/main/java/com/example/uptoyou/UI/PreferencesActivity.java
@@ -0,0 +1,15 @@
+package com.example.uptoyou.UI;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PreferencesActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@NonNull Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    
+}
